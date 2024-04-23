3880a2476acbb6e49b06025669eb79b1e490f53b
Bhumika gupta <bg0674079@gmail.com>
2024-04-23 18:02:29 +0530

    route

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
+import Entry from './Components/Entry';
+import Home from './Components/Home';
+import Library from './Components/Library'
+import Nav from './Components/Nav'
+
+const App = () => {
+  return (
+    <div>
+      <Nav/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/create" element={<Entry />} />
+        <Route path="/show" element={<Library/>} />
+        <Route path = "*" element={<h1>PAge Not FOund</h1>} />
+      </Routes>
+    </div>
+  )
+}
+
+export default App

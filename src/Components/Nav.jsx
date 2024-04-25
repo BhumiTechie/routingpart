72ca4fc5e3ae7aca83e383caa3a96da60b293a89
Bhumika gupta <bg0674079@gmail.com>
2024-04-25 16:39:29 +0530

    gap

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -3,13 +3,14 @@ import { NavLink} from 'react-router-dom'
 
 const Nav = () => {
   return (
-	<div className='w-full flex justify-center gap-5 p-3'>
-      <nav>
-		<NavLink className = {(e)=> (e.isActive ? "text-red-300" : "")} to = "/">Home</NavLink>
-		<NavLink  className = {(e) => (e.isActive ? "text-red-300" : "")} to= "/create">Book Entry</NavLink>
-		<NavLink  className = {(e) => (e.isActive ? "text-red-300" : "")} to = "/show">Library</NavLink>
- </nav>
-	</div>
+<div className='w-full flex justify-center p-3'>
+  <nav className="flex gap-5">
+    <NavLink className={(e) => (e.isActive ? "text-red-300" : "")} to="/">Home</NavLink>
+    <NavLink className={(e) => (e.isActive ? "text-red-300" : "")} to="/create">Book Entry</NavLink>
+    <NavLink className={(e) => (e.isActive ? "text-red-300" : "")} to="/show">Library</NavLink>
+  </nav>
+</div>
+
   )
 }
 

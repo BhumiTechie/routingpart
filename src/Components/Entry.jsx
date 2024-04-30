d866827cb1a5d019086b2da19152fea2f2a02417
Bhumika gupta <bg0674079@gmail.com>
2024-04-30 13:00:38 +0530

    get

--- src/Components/Entry.jsx
+++ src/Components/Entry.jsx
@@ -1,4 +1,5 @@
 
+import axios from 'axios';
 import { useNavigate } from 'react-router-dom'
 
 const Entry = () => {
@@ -8,9 +9,22 @@ const Entry = () => {
     navigate("/show");
     //navigate(1);
     //navigate(-1)
+     //  eslint-disable-next-line no-unused-vars
+const getposts = async ()=>{
+  try{
+    const {data} = await axios.get(`/posts`);
+    console.log(data);
+  }catch(e){
+    console.log(e);
+  }
+}
+
   }
   return (
 	<div>
+    <h1 className='text-2xl font-extrabold'>Book Entries</h1>
+    <button className='bg-green-300 py-2 px-5 my-3' onClick={getposts}>GET POST</button>
+    <br/> <br/>
     <button onClick={SubmitHandler} className='bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded m-3'> Submit</button>
   </div>
   )
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,35 @@
-// import React from 'react'
+
+import axios from "axios";
+import { useState } from "react";
 
 const Home = () => {
+  const[ users , setusers] = useState([])
+  //  eslint-disable-next-line no-unused-vars
+  const getusers = async() =>{
+    try {                // try catch using for error handling
+      const { data } = await axios.get("/users") 
+    setusers(data);
+    console.log(data);
+    } catch (error){
+      console.log(error)
+    }
+     };
+
+     let renderusers = "Loading users...";
+     if(users.length > 0){
+        //  eslint-disable-next-line no-unused-vars
+        
+        renderusers = users.map((user)=>{
+        <div className="bg-red-100 p-3 mr-2 mb-2" key={user.id}>
+          <h1 className="font-2xl">{user.name}</h1>
+          <small>{user.email}</small>
+        </div>
+      })
+     }
   return (
 	<div>
     HEY THIS IS A HOME PAGE!
+    <button className="w-28 p-3 mb-3 text-white bg-sky-400 m-5" onClick={getusers}>Get users</button> 
   </div>
   )
 }
